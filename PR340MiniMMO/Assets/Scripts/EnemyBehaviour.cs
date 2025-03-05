98d9b183fbfbf13c284b26df58997adc0c302771
mikelweber03 <mikelweber03@gmail.com>
2025-03-05 21:46:19 +0100

    Enemies move towards player
    using navmesh, navmesh agent and nav.SetDestination

--- PR340MiniMMO/Assets/Scripts/EnemyBehaviour.cs
+++ PR340MiniMMO/Assets/Scripts/EnemyBehaviour.cs
@@ -1,12 +1,68 @@
+using UnityEngine;
 using Unity.Netcode;
- using UnityEngine;
+using Unity.VisualScripting;
+using UnityEngine.AI;
+using UnityEngine.Animations;
+
 public class EnemyBehaviour : NetworkBehaviour
 {
+    [SerializeField] private float moveSpeed = 3f;
 
-    public override void OnNetworkSpawn()
+    private NetworkVariable<Vector3> targetPosition = new NetworkVariable<Vector3>();
+    UnityEngine.AI.NavMeshAgent enemyAgent;
+
+    private void Start()
     {
-        base.OnNetworkSpawn();
+        enemyAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+    private void Update()
+    {
+ 
+        if (!IsServer) return;
 
-        Debug.Log("Spawned enemy");
+        // Find nearest player
+        GameObject nearestPlayer = FindNearestPlayer();
+        print("chasing1");
+        if (nearestPlayer != null)
+        {
+            // Move towards the player
+            enemyAgent.SetDestination(nearestPlayer.transform.position);
+            //Vector3 direction = (nearestPlayer.transform.position - transform.position).normalized;
+            //targetPosition.Value = transform.position + direction * moveSpeed * Time.deltaTime;
+            //transform.position = targetPosition.Value;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //   // Check if enemy touches a player
+        //   GameObject player = other.GameObject();
+        //   if (player != null)
+        //   {
+        //       // Eliminate player, gotta set each player active again each round
+        //       player.SetActive(false);
+        //   }
+        print("Player has been tagged");
+    }
+
+    private GameObject FindNearestPlayer()
+    {
+        
+        print("chasing2");
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        GameObject nearest = null;
+        float minDistance = float.MaxValue;
+
+        foreach (GameObject player in players)
+        {
+            float distance = Vector3.Distance(transform.position, player.transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                nearest = player;
+            }
+        }
+
+        return nearest;
     }
 }
--- PR340MiniMMO/Assets/Scripts/EnemySpawner.cs
+++ PR340MiniMMO/Assets/Scripts/EnemySpawner.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private EnemyBehaviour enemy;
-
     void Start()
     {
         InvokeRepeating(nameof(SpawnEnemy), 2f, 15f);
