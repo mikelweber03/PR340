b9386b2d082db94dfc4f90cc7f4739e16ebd4193
mikelweber03 <mikelweber03@gmail.com>
2025-03-06 16:00:05 +0100

    Players can see each other moving
    Used an extra Netcode package https://github.com/Unity-Technologies/com.unity.multiplayer.samples.coop?path=%2FPackages%2Fcom.unity.multiplayer.samples.coop#main

--- PR340MiniMMO/Assets/Scripts/MovementNetworkController.cs
+++ PR340MiniMMO/Assets/Scripts/MovementNetworkController.cs
@@ -7,10 +7,16 @@ using UnityEngine;
 public class MovementNetworkController : NetworkBehaviour
 {
     public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+    [SerializeField]
+    public float speed = 5f;
 
     [ServerRpc]
     void SubmitPositionRequestServerRpc(Vector3 position, ServerRpcParams serverRpcParams = default) => Position.Value = position;
 
+    //Thought this would sync the movements in each players build, so one player can see the other moving
+   // [ClientRpc]
+   // void SubmitPositionRequestClientRpc(Vector3 position, ClientRpcParams clientRpcParams = default) => Position.Value = position;
+        
     void Update()
     {
         
@@ -18,9 +24,12 @@ public class MovementNetworkController : NetworkBehaviour
         {
             float moveX = Input.GetAxis("Horizontal");
             float moveZ = Input.GetAxis("Vertical");
-            Vector3 movement = new Vector3(moveX, 0f, moveZ) * 5 * Time.deltaTime;
+            Vector3 movement = new Vector3(moveX, 0f, moveZ) * speed * Time.deltaTime;
             transform.Translate(movement, Space.World);
             SubmitPositionRequestServerRpc(transform.position);
+            
+            //SubmitPositionRequestClientRpc(transform.position);
+            
         }
         if (IsServer)
             transform.position = Position.Value;
--- PR340MiniMMO/Assets/Scripts/PlayerCamera.cs
+++ PR340MiniMMO/Assets/Scripts/PlayerCamera.cs
@@ -6,21 +6,39 @@ using Unity.Netcode;
 using Unity.Mathematics;
 public class PlayerCamera : NetworkBehaviour
 {
-    
-    [SerializeField]
-    private GameObject player;
-    public Vector3 offset;
-    public quaternion rotation;
-   
-    public override void OnNetworkSpawn()
-    { // This is basically a Start method
+    // 
+    // [SerializeField]
+    // private GameObject player;
+     public Vector3 offset;
+     public Quaternion rotation;
+    //
+    // public override void OnNetworkSpawn()
+    // { // This is basically a Start method
+    //     if (IsOwner)
+    //     {
+    //         GetComponent<Camera>().enabled = true;
+    //     }
+    //     else
+    //     {
+    //         GetComponent<Camera>().enabled = false;
+    //     }
+    // }
+    //
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     transform.position = player.transform.position + offset;
+    //     transform.rotation = rotation;
+    // }
 
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        transform.position = player.transform.position + offset;
-        transform.rotation = rotation;
+        if(IsOwner)
+        {
+            GameObject.FindGameObjectWithTag("MainCamera").gameObject.transform.parent = transform;
+            GameObject.FindGameObjectWithTag("MainCamera").gameObject.transform.position = GameObject.FindGameObjectWithTag("MainCamera").gameObject.transform.position + offset;
+            GameObject.FindGameObjectWithTag("MainCamera").gameObject.transform.rotation = rotation;
+        }
+
     }
 }
